826ffa685b923c71452cdaf7c3d28da7858b99a2
cuanboy <1037770675@qq.com>
2017-12-14 13:17:29 +0800

    scrapy初始Url的两种写法

--- simpleStartUrl/simpleStartUrl/spiders/simpleUrl.py
+++ simpleStartUrl/simpleStartUrl/spiders/simpleUrl.py
@@ -1,10 +1,9 @@
 """
-    初始链接的两种写法，
+    scrapy初始Url的两种写法，
     一种是常量start_urls，并且需要定义一个方法parse（）
     另一种是直接定义一个方法：star_requests()
 """
 import scrapy
-
 
 class simpleUrl(scrapy.Spider):
     name = "simpleUrl"
